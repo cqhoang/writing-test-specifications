7f65a5cb050013e386bb7bd0d9e05a3604211e00
Christina <christinah.0821@gmail.com>
2024-03-04 18:59:01 -0800

    prompts

---
+++ index.js
@@ -0,0 +1,21 @@
+// Prompt 1: a function called "multiplication" that returns the product of the two input numbers
+// Expect multiplication(2, 3) to be a number
+// Expect multiplication(2, 3) to be equal to 6
+// Expect multiplication("a", 3) to be an error
+
+// Prompt 2: A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+// Expect concatOdds to return a string of odds
+// Expect concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to result in [-1, 1, 3, 9, 15]
+// Expect concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to result in [2, 4] to be an error
+// Expect concatOdds to not return a string of evens
+
+// Prompt 3: A shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
+// Expect a shopping cart checkout feature to allow a user to check out as a guest or as a logged-in user
+// Expect guest to be asked to create an account or log in
+// Expect a shopping cart checkout feature to have a checkout button
+// Expect guest to click "sign up" to create an account
+// Expect guest to click an option
+// Expect guest to not be able to skip
+// Expect guest to input invalid email address or password, to result in an error display
+// Expect guest to click "log in" but not fill out login or password and to be shown an error and prompt to "sign up"
+// Expect feature to display error if cart is empty and guest attempts to check out
